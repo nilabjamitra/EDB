0e99ba6ac05a3275de85e9d023d4067b27e64380
Nilabja_07 <115585586+nilabjamitra@users.noreply.github.com>
2024-10-20 20:44:36 +0530

    Add files via upload

---
+++ Inventory_Management_System/src/main/java/com/mycompany/inventory_management_system/Inventory_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventory_management_system;
+
+/**
+ *
+ * @author Shreya
+ */
+public class Inventory_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
